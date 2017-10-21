fed0c2924b202953324c44b61ecd178b0c260046
Hilmar Tryggvason <hilmart@gmail.com>
2017-10-21 22:29:38 +0000

    Created Test project

---
+++ src/Tests/Services/UserServiceTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryAPI.Services;
+using LibraryAPI.Interfaces.Services;
+
+namespace LibraryAPI.Tests.Services
+{
+    [TestClass]
+    public class UserServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
